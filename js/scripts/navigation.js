809c071b265636a0e92cef8dca50c41f57e5055a
Leandro <uideveloper@figueiredo.dev>
2022-11-07 18:08:34 -0300

    improve navigation menu

--- js/scripts/navigation.js
+++ js/scripts/navigation.js
@@ -1,9 +1,18 @@
-const menuItens = document.querySelectorAll('[data-menu-item]');
 const menuPointer = document.querySelector('[data-menu-pointer]');
+const sections = document.querySelectorAll('[data-section]');
+let currentOffsetTop, topDistance, currentSectionName;
 
-menuItens.forEach((item) => {
-    item.addEventListener('click', () => {
-        let topDistance = item.offsetTop;
-        menuPointer.style.top = `${topDistance}px`;
+const navigateMenu = () => {
+    sections.forEach((item) => {
+        currentOffsetTop = Math.trunc(window.scrollY - item.offsetTop);
+        
+        if((currentOffsetTop > -200) && (currentOffsetTop < 200)) {
+            currentSectionName = item.getAttribute('data-section');
+            topDistance = document.querySelector(`[data-menu-item="${currentSectionName}"]`).offsetTop;
+
+            menuPointer.style.top = `${topDistance}px`;
+        }
     })
-})
+};
+
+window.addEventListener('scroll', navigateMenu);
