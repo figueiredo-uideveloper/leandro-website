b593df3a9feee60dd2b1cfcb481028a50787a120
Leandro <leandro@Leandros-MacBook-Pro.local>
2022-11-09 02:20:19 -0300

    style tag html

--- js/scripts/switch-theme.js
+++ js/scripts/switch-theme.js
@@ -1,6 +1,7 @@
 const switchTheme = document.querySelector('[data-switch-theme]');
 let inputCheckbox = document.querySelector('.toggle-switch');
 const tagBody = document.querySelector('body');
+const tagHtml = document.querySelector('html');
 
 const toggleTheme = () => {
     const currentMode = switchTheme.getAttribute('data-switch-theme');
@@ -10,12 +11,14 @@ const toggleTheme = () => {
         switchTheme.setAttribute('data-switch-theme', 'light');
         tagBody.setAttribute('data-theme', 'light-mode');
         inputCheckbox.checked = false;
+        tagHtml.style.background = '#000';
     }
 
     if (currentMode === 'light') {
         switchTheme.setAttribute('data-switch-theme', 'dark');
         tagBody.setAttribute('data-theme', 'dark-mode');
         inputCheckbox.checked = true;
+        tagHtml.style.background = '#FFF';
     }
 
 }
